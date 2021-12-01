b870f78d6de2dad167ad1801c8c894df7d511087
Aneudys Germosen <aneudysgermosen@HAL-9000.lan>
2021-12-01 11:35:39 -0500

    added instruction screen

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,11 +2,15 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g3d.particles.influencers.SpawnInfluencer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.Random;
@@ -17,17 +21,33 @@ public class MyGdxGame extends Game {
 	//GameScreen gameScreen;
 	public static Random rand = new Random();
 	SpriteBatch batch;
+	SpriteBatch instructionBatch;
 	ShapeRenderer shapeRenderer;
+
 	BitmapFont font;
+	BitmapFont title;
+	BitmapFont instructions;
+
+	//Camera camera;
+	public Texture ins;
+	Rectangle instructionRect;
 
 	@Override
 	public void create() {
 		//gameScreen = new GameScreen();
 		//setScreen(gameScreen);
 		batch = new SpriteBatch();
+		ins = new Texture("instruction.png");
+		//
 		shapeRenderer = new ShapeRenderer();
 		font = new BitmapFont();
+		title = new BitmapFont();
+		instructions = new BitmapFont();
 		setScreen(new titleScreen(this));
+		//setScreen(new instructionScreen());
+		instructionRect = new Rectangle(Gdx.graphics.getWidth() * .20f,
+				Gdx.graphics.getHeight() * .65f, ins.getWidth(), ins.getHeight());
+
 	}
 
 	@Override
