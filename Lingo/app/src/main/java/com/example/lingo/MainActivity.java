085ac3d9ab24259fb90f001ee58e8c8e7cff53e1
Aneudys Germosen <aneudysgermosen@HAL-9000.local>
2021-12-07 15:40:16 -0500

    added music to game

--- Lingo/app/src/main/java/com/example/lingo/MainActivity.java
+++ Lingo/app/src/main/java/com/example/lingo/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lingo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +12,13 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button instructions;
+    MediaPlayer click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        click = MediaPlayer.create(this, R.raw.game_music);
 
         instructions = (Button) findViewById(R.id.instrcutionButton);
 
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, activity_instruction.class);
+                click.start();
                 startActivity(intent);
             }
         });
