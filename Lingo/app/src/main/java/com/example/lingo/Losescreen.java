c4a11baeee08fd7ad2fe96f38609d3adcec14a89
Spiros Rally <srally1@binghamton.edu>
2021-12-17 23:34:53 -0500

    'win+lose'

--- Lingo/app/src/main/java/com/example/lingo/Losescreen.java
+++ Lingo/app/src/main/java/com/example/lingo/Losescreen.java
@@ -1,4 +1,29 @@
 package com.example.lingo;
 
-public class Losescreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Losescreen extends AppCompatActivity {
+
+    Button goBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lose);
+
+        goBack = (Button)findViewById(R.id.Back);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_instruction.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
--- Lingo/app/src/main/java/com/example/lingo/Winscreen.java
+++ Lingo/app/src/main/java/com/example/lingo/Winscreen.java
@@ -1,4 +1,29 @@
 package com.example.lingo;
 
-public class Winscreen {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Winscreen extends AppCompatActivity {
+
+    Button goBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.win);
+
+        goBack = (Button)findViewById(R.id.Back);
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity_instruction.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
